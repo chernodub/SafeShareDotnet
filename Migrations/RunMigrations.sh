da88b277c650719bec05ffe6c77eddc30d259298
Viktor Chernodub <chernodubv@gmail.com>
2023-01-17 14:22:08 +0800

    feat: automate migrations

--- Migrations/RunMigrations.sh
+++ Migrations/RunMigrations.sh
@@ -1,2 +1,3 @@
+# To run the migrations manually
 source ./.env && 
 dotnet ef database update --connection "Server=localhost;Port=${POSTGRES_PORT};Database=${POSTGRES_DB};User Id=${POSTGRES_USER};Password=${POSTGRES_PASSWORD};" --context $1
